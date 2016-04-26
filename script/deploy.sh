8aeeadb9248922e68868851609472f3d23e3e5a0
Santhosh <santhoshinet@gmail.com>
2016-04-26 11:42:37 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,13 @@
-#! /bin/bash
-# script/deploy.sh
+TAG=deployed_at_$(date +"%F_%H-%M")
+git tag -m '' -a $TAG
+git push --tags
  
-bundle exec rake db:migrate db:seed assets:clean assets:precompile
+ssh user@your_domain.com << 'SSH'
+  cd /var/rails_apps/my_app
+  rm -rf public/assets
+  git pull
+  bundle install --without development test
+  bundle exec rake db:migrate db:seed assets:clean assets:precompile
+  touch tmp/restart.txt
+  git describe > public/version
+SSH
